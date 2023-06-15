2a812fb13265f3a97ede1f9a9f661039669be298
David <daveman@gmail.com>
2023-06-15 11:26:39 -0700

    completed building components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import Alert from './components/Alert';
 import Button from './components/Button';
+import { useState } from 'react';
 
 function App() {
+  const [showAlert, setShowAlert] = useState(false);
+
   return (
     <div>
-      <Alert>Hello World</Alert>
-      <Button onClick={() => console.log('clicked')} color='danger'>
-        Woof
+      {showAlert && <Alert onClose={() => setShowAlert(false)}>My alert</Alert>}
+      <Button onClick={() => setShowAlert(true)} color='secondary'>
+        {/*passing setShowAlert as a fat arrow function ensures that the function
+      is not immediately called but is only executed when the button is
+      clicked.*/}
+        My Button
       </Button>
     </div>
   );
