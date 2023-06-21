6ce8bafdf81f9196b01403639045b7cda5b428ae
David <daveman@gmail.com>
2023-06-20 20:19:01 -0700

    added expense form

--- src/expense-tracker/components/ExpenseFilter.tsx
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { categories } from "../../App";
 
 interface Props {
   onSelectCategory: (category: string) => void;
@@ -6,13 +7,18 @@ interface Props {
 
 const ExpenseFilter = ({ onSelectCategory }: Props) => {
   return (
-    <select className="form-select" onChange={ (event) => onSelectCategory(event.target.value)}>
+    <select
+      className="form-select"
+      onChange={(event) => onSelectCategory(event.target.value)}
+    >
       <option value="">All categories</option>
-      <option value="Groceries">Groceries</option>
-      <option value="Utilities">Utilities</option>
-      <option value="Entertainment">Entertainment</option>
+      {categories.map((category) => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
     </select>
   );
 };
 
-export default ExpenseFilter
+export default ExpenseFilter;
