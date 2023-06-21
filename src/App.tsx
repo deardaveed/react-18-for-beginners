01987637d2c3d9670d2e0acc4fd2b2d6d6965b99
David <daveman@gmail.com>
2023-06-20 21:15:59 -0700

    completed expense tracker

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,29 @@ import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import categories from "./expense-tracker/categories";
 
 function App() {
-	const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 10, category: "Utilities" },
     { id: 3, description: "ccc", amount: 10, category: "Utilities" },
     { id: 4, description: "ddd", amount: 10, category: "Utilities" },
-	]);
+  ]);
 
-	const visibleExpenses = selectedCategory ? expenses.filter(e => e.category === selectedCategory) : expenses;
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   return (
-		<div>
-			<div className="mb-3">
-				<div className="mb-5">
-					<ExpenseForm />
-				</div>
-				<ExpenseFilter onSelectCategory={category => setSelectedCategory(category)} />
-			</div>
+    <div>
+      <div className="mb-3">
+        <div className="mb-5">
+          <ExpenseForm onSubmit={ expense => setExpenses([...expenses, {...expense, id: expenses.length+1}])} />
+        </div>
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
         expenses={visibleExpenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
--- src/expense-tracker/components/ExpenseForm.tsx
+++ src/expense-tracker/components/ExpenseForm.tsx
@@ -4,52 +4,85 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
 
-
 const schema = z.object({
-  description: z.string().min(3, {message: 'Description should be at least 3 characters'}).max(50),
-	amount: z.number({ invalid_type_error: 'Amount is required'}).min(0.01).max(100_00),
-	category: z.enum(categories, {
-		errorMap: ()=> ({message: 'Category is required.'})
-	}),
+  description: z
+    .string()
+    .min(3, { message: "Description should be at least 3 characters" })
+    .max(50),
+  amount: z
+    .number({ invalid_type_error: "Amount is required" })
+    .min(0.01)
+    .max(100_00),
+  category: z.enum(categories, {
+    errorMap: () => ({ message: "Category is required." }),
+  }),
 });
 
 type ExpenseFormData = z.infer<typeof schema>;
 
-const ExpenseForm = () => {
+interface Props {
+  onSubmit: (data: ExpenseFormData) => void;
+}
+
+const ExpenseForm = ({ onSubmit }: Props) => {
   const {
     register,
-    handleSubmit,
+		handleSubmit,
+		reset,
     formState: { errors },
   } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
   return (
-    <form onSubmit={handleSubmit(data => console.log(data))}>
+		<form onSubmit={handleSubmit(data => {
+			onSubmit(data);
+			reset();
+		})}>
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
         </label>
-				<input {...register('description')} id="description" type="text" className="form-control" />
-				{errors.description && <p className='text-danger'>{errors.description.message}</p>}
+        <input
+          {...register("description")}
+          id="description"
+          type="text"
+          className="form-control"
+        />
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
-				<input {...register('amount', {valueAsNumber: true})} id="amount" type="number" className="form-control" />
-					{errors.amount && <p className='text-danger'>{errors.amount.message}</p>}
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          id="amount"
+          type="number"
+          className="form-control"
+        />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="category" className="form-label">
           Category
         </label>
-				<select {...register('category')} id="category" name="" className="form-select">
+        <select
+          {...register("category")}
+          id="category"
+          className="form-select"
+        >
           <option value=""></option>
           {categories.map((category) => (
             <option key={category} value={category}>
               {category}
             </option>
           ))}
-				</select>
-					{errors.category && <p className='text-danger'>{errors.category.message}</p>}
+        </select>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
